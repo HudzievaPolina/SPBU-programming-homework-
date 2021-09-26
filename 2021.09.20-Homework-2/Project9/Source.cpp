953fea29db8e2791b80316c59f7a2aaab9a1daea
HudzievaPolina <p.hirundo@gmail.com>
2021-09-26 13:48:42 +0300

    project 9 code changes

--- 2021.09.20-Homework-2/Project9/Source.cpp
+++ 2021.09.20-Homework-2/Project9/Source.cpp
@@ -14,31 +14,48 @@ int main(int argc, char* argv[])
 	{
 		if (y == 1)
 		{
-			cout << x + 1 << ", " << y << "; " << x + 1 << ", " << y + 1 << "; " << x << ", " << y + 1 << endl;
+			cout << x + 1 << " " << y << endl;
+			cout << x << " " << y + 1 << endl;
 		}
 		else
 		{
-			cout << x + 1 << ", " << y << "; " << x + 1 << ", " << y + 1 << "; " << x << ", " << y + 1 << "; " << x + 1 << ", " << y - 1 << "; " << x << ", " << y - 1 << endl;
+			cout << x + 1 << " " << y << endl; 
+			cout << x << " " << y + 1 << endl;
+			cout << x << " " << y - 1 << endl; 
 		}
 	}
-	else if (y == 1)
+	else if (y == 1 && x != 1)
 	{
-		cout << x + 1 << ", " << y << "; " << x + 1 << ", " << y + 1 << ", " << x << ", " << y + 1 << "; " << x - 1 << ", " << y + 1 << ", " << x - 1 << ", " << y << endl;
+		cout << x + 1 << " " << y << endl;
+		cout << x << " " << y + 1 << endl; 
+		cout << x - 1 << " " << y << endl;
 	}
 	if (x == 109)
 	{
 		if (y == 109)
 		{
-			cout << x - 1 << ", " << y << "; " << x - 1 << ", " << y - 1 << "; " << x << ", " << y - 1 << endl;
+			cout << x - 1 << " " << y << endl;
+			cout << x << " " << y - 1 << endl;
 		}
 		else
 		{
-			cout << x - 1 << ", " << y << "; " << x - 1 << ", " << y - 1 << "; " << x << ", " << y - 1 << "; " << x << ", " << y + 1 << "; " << x - 1 << ", " << y + 1 << endl;
+			cout << x - 1 << " " << y << endl;
+			cout << x << " " << y - 1 << endl;
+			cout << x << " " << y + 1 << endl;
 		}
+	}
+	else if (y == 109 && x != 109)
+	{
+		cout << x + 1 << " " << y << endl;
+		cout << x << " " << y - 1 << endl;
+		cout << x - 1 << " " << y << endl;
 	}
 	else
 	{
-		cout << x + 1 << ", " << y << "; " << x + 1 << ", " << y + 1 << "; " << x << ", " << y + 1 << "; " << x + 1 << ", " << y - 1 << "; " << x << ", " << y - 1 << "; " << x - 1 << "," << y - 1 << "; " << x - 1 << ", " << y << "; " << x - 1 << ", " << y + 1 << endl;
+		cout << x + 1 << " " << y << endl;
+		cout << x << " " << y + 1 << endl;
+		cout << x - 1 << " " << y << endl; 
+		cout << x << " " << y - 1 << endl; 
 	}
 	return EXIT_SUCCESS;
 }
